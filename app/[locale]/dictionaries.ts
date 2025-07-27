cdf7d65eeb5c41e8df771ff6b5139eb261f79f12
ZAHRAN88 <palastinearabia@gmail.com>
2025-07-27 20:47:54 +0300

    Create dictionaries.ts

---
+++ app/[locale]/dictionaries.ts
@@ -0,0 +1,10 @@
+import 'server-only';
+
+const dictionaries = {
+  en: () => import('../../messages/en.json').then((module) => module.default),
+  fr: () => import('../../messages/fr.json').then((module) => module.default),
+  ar: () => import('../../messages/ar.json').then((module) => module.default),
+};
+
+export const getDictionary = async (locale: 'en' | 'fr' | 'ar') =>
+  dictionaries[locale](); 
